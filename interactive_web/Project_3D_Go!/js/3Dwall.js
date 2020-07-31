732a3d0cac278d8e14a836709e724f3338f97ac5
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-07-31 15:06:27 +0900

    Remove comment

--- interactive_web/Project_3D_Go!/js/3Dwall.js
+++ interactive_web/Project_3D_Go!/js/3Dwall.js
@@ -1,6 +1,6 @@
 (function() {
-    const stageElem = document.querySelector('.stage');
     const houseElem = document.querySelector('.house');
+    const stageElem = document.querySelector('.stage');
     let maxScrollValue;
 
     function resizeHandler() {
@@ -10,10 +10,17 @@
     function scrollHandler() {
         const scrollPer = pageYOffset / maxScrollValue;
         const zMove = scrollPer * 980 - 490;
-        console.log(zMove);
         houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
 
     }
+
+    function mouseHandler(e) {
+        const pointX = -1 + (e.clientX / window.innerWidth) * 2;
+        const pointY = 1 - (e.clientY / window.innerHeight) * 2;
+        console.log(pointX, pointY)
+        stageElem.style.transform = 'rotateY(' + pointX * 7 + 'deg) rotateX(' + pointY * 7 + 'deg)'
+    }
+    window.addEventListener('mousemove', mouseHandler);
     window.addEventListener('scroll', scrollHandler);
     resizeHandler();
 })();
