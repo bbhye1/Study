1688526ff277ceb66f5b6585539a043081630b43
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-08-06 14:44:36 +0900

    Update script.js
    Get browser local storage information and populate UI

--- 20 Web Project With Vanilla Javascript/movie-seat-booking/script.js
+++ 20 Web Project With Vanilla Javascript/movie-seat-booking/script.js
@@ -5,6 +5,8 @@
     const count = document.getElementById('count');
     const total = document.getElementById('total');
 
+    populateUI();
+
     let moviePrice = +movie.value;
 
 
@@ -20,25 +22,46 @@
 
         const seatsIndex = [...selectedSeat].map(seat => [...selected].indexOf(seat));
         localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
         const selectedSeatCount = +selectedSeat.length;
 
         count.innerText = selectedSeatCount;
         total.innerText = selectedSeatCount * moviePrice;
     }
 
+    // Get data form localstorage and populate UI
+    function populateUI() {
+        const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+        if (selectedSeats !== null && selectedSeats.length > 0) {
+            selected.forEach((seat, index) => {
+                if (selectedSeats.indexOf(index) > -1) {
+                    seat.classList.add('selected');
+                }
+            });
+        }
+        const selectedMovieIndex = JSON.parse(localStorage.getItem('selectedMovieIndex'));
+        if (selectedMovieIndex !== null) {
+            movie.selectedIndex = selectedMovieIndex;
+        }
+    }
+
+
     // Movie select event
     movie.addEventListener('change', e => {
         moviePrice = +e.target.value;
-        updateSelectedCount();
-        console.log(e.target.selectedIndex);
         setMovieData(e.target.selectedIndex, e.target.value);
-    })
+        updateSelectedCount();
+    });
 
     // Seat click event
     container.addEventListener('click', e => {
         if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
             e.target.classList.toggle('selected');
+            updateSelectedCount();
         }
-        updateSelectedCount();
-    })
+
+    });
+
+    //Initial count and total set
+    updateSelectedCount();
 })();
