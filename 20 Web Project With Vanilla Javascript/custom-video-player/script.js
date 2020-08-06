6915fa7a188ea2b45beb2026eb33f27cdde84315
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-08-06 17:07:19 +0900

    Update script.js
    Make video screen , play & pause & stop button work.

---
+++ 20 Web Project With Vanilla Javascript/custom-video-player/script.js
@@ -0,0 +1,54 @@
+(function() {
+    const video = document.getElementById('video');
+    const play = document.getElementById('play');
+    const stop = document.getElementById('stop');
+    const progress = document.getElementById('progress');
+    const timestamp = document.getElementById('timestamp');
+    let currentTime = 0;
+
+    // play / pause video
+    function toggleVideoStatus() {
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    }
+
+    // update play / pause  video
+    function updatePlayIcon() {
+        if (video.paused) {
+            play.innerHTML = `<i class="fa fa-pause fa-2x"></i>`
+        } else {
+            play.innerHTML = `<i class="fa fa-play fa-2x"></i>`
+        }
+    }
+
+    // update Progress Timestamp 
+    function updateProgress() {
+
+    }
+
+    //set video time to progress 
+    function setVideoProgress(e) {
+        return true;
+    }
+
+    // stop video
+    function stopVideo() {
+        video.currentTime = 0;
+        video.pause();
+    }
+
+    // Event Listener
+    video.addEventListener('click', toggleVideoStatus);
+    video.addEventListener('pause', updatePlayIcon);
+    video.addEventListener('play', updatePlayIcon);
+    video.addEventListener('timeupdate', updateProgress);
+
+    play.addEventListener('click', toggleVideoStatus);
+
+    stop.addEventListener('click', stopVideo);
+
+    progress.addEventListener('change', setVideoProgress);
+})();
