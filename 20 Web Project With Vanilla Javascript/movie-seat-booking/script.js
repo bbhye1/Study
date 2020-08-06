61d5e4ca93f5f1edd2350846a52a3e91793fe31f
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-08-06 13:56:22 +0900

    Update script.js
    Save the selected movie index, seats index, and price into browser local storage.

--- 20 Web Project With Vanilla Javascript/movie-seat-booking/script.js
+++ 20 Web Project With Vanilla Javascript/movie-seat-booking/script.js
@@ -7,18 +7,35 @@
 
     let moviePrice = +movie.value;
 
+
+    // Save selected movie index and price
+    function setMovieData(movieIndex, moviePrice) {
+        localStorage.setItem('selectedMovieIndex', movieIndex);
+        localStorage.setItem('selectedMoviePrice', moviePrice);
+    }
+
+    // Update total and count
     function updateSelectedCount() {
         const selectedSeat = document.querySelectorAll('.row .seat.selected');
+
+        const seatsIndex = [...selectedSeat].map(seat => [...selected].indexOf(seat));
+        localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
         const selectedSeatCount = +selectedSeat.length;
 
         count.innerText = selectedSeatCount;
         total.innerText = selectedSeatCount * moviePrice;
     }
-    window.addEventListener('change', e => {
+
+    // Movie select event
+    movie.addEventListener('change', e => {
         moviePrice = +e.target.value;
         updateSelectedCount();
+        console.log(e.target.selectedIndex);
+        setMovieData(e.target.selectedIndex, e.target.value);
     })
-    window.addEventListener('click', e => {
+
+    // Seat click event
+    container.addEventListener('click', e => {
         if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
             e.target.classList.toggle('selected');
         }
