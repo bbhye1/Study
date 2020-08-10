37e92a3e4766ef3c32d5ff9f622bb058fb259c17
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-08-10 15:30:39 +0900

    Create keydown Event listener
    Create keydown event  lister to show  the word you press is correct or not.
    and display notification if duplicates.

--- 20 Web Project With Vanilla Javascript/hangman/script.js
+++ 20 Web Project With Vanilla Javascript/hangman/script.js
@@ -30,5 +30,38 @@
                 }  
     }
 
+
+    //Keydown event
+    window.addEventListener('keydown', e => {
+        if(e.keyCode >= 65 && e.keyCode <= 90) {
+            const letter = e.key;
+          if(selectedWord.includes(letter)) {
+             if(!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+                displayWord()
+             } else {
+                showNotification();
+             }
+          } else {
+            if(!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter);
+                displayWord()
+            } else {
+               showNotification();
+            }
+         }
+        }
+    })
+
+    // Show notification 
+    function showNotification() {
+        notification.classList.add('show'); 
+
+        setTimeout(()=>{
+            notification.classList.remove('show')},2000);
+    }
+
+    
+
     displayWord();
 })();
