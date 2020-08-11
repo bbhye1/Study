32683227cbe6fcbca70fa41e10023c06b4a6303c
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-08-11 14:17:38 +0900

    update add transaction function and delete button function
    Use submit event listener to add new transaction.
    and add button function to delete selected transaction.
    and correct the misspelled word in html.

--- 20 Web Project With Vanilla Javascript/expense-tracker/script.js
+++ 20 Web Project With Vanilla Javascript/expense-tracker/script.js
@@ -1,62 +1,99 @@
 "use strict";
 
-(function() {
-    const balance = document.getElementById('balance');
-    const moneyPlus = document.getElementById('money-plus');
-    const moneyMinus = document.getElementById('money-minus');
-    const list = document.getElementById('list');
-    const form = document.getElementById('form');
-    const amount = document.getElementById('amount');
-    const text = document.getElementById('text');
 
-    const testTransaction = [
-        { id: 1, text: 'Flower', amount: -20 },
-        { id: 2, text: 'Salary', amount: 300 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 }
-    ];
+const balance = document.getElementById('balance');
+const moneyPlus = document.getElementById('money-plus');
+const moneyMinus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const amount = document.getElementById('amount');
+const text = document.getElementById('text');
 
-    let transactions = testTransaction;
+const testTransaction = [
+    { id: 1, text: 'Flower', amount: -20 },
+    { id: 2, text: 'Salary', amount: 300 },
+    { id: 3, text: 'Book', amount: -10 },
+    { id: 4, text: 'Camera', amount: 150 }
+];
 
-    // Add trasactions to DOM list
-    function addTransactionDOM(transaction) {
-        // GEt sign
-        const sign = transaction.amount < 0 ? "-$" : "+$"
-        const historyList = document.createElement('li');
+let transactions = testTransaction;
 
-        // Add class based on value
-        historyList.classList.add(transaction.amount < 0 ? "minus" : "plus");
-        historyList.innerHTML = `${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span><button class="delete-btn">x</button>`
+// Add new transaction
+function addTransaction(e) {
+    e.preventDefault();
 
-        list.appendChild(historyList);
-    }
+    if (amount.value.trim() === '' || text.value.trim() === '') {
+        alert('Please add a text and amount');
+    } else {
+        const transaction = {
+            id: generateID(),
+            text: text.value,
+            amount: +amount.value
+        };
 
-    // Display balance, income and expense
-    function updateValues() {
-        const amount = transactions.map(item => item.amount);
-        const value = amount.reduce((acc, item) => (acc += item), 0);
-        const income = amount
-            .filter((item) => item > 0)
-            .reduce((acc, item) => (acc += item), 0)
-            .toFixed(2);
-        const expense = (amount
-                .filter((item) => item < 0)
-                .reduce((acc, item) => (acc += item), 0) * -1)
-            .toFixed(2);
-
-        balance.innerText = `$${value}`;
-        moneyPlus.innerText = `$${income}`;
-        moneyMinus.innerText = `$${expense}`;
-
-    }
-
-    // Init  app
-    function init() {
-        list.innerHTML = ''
-
-        transactions.forEach(addTransactionDOM);
+        transactions.push(transaction);
+        addTransactionDOM(transaction);
         updateValues();
-    }
 
+        text.value = '';
+        amount.value = '';
+    }
+}
+
+//Get random transaction ID
+function generateID() {
+    return Math.floor(Math.random() * 100000000);
+}
+
+// Add trasactions to DOM list
+function addTransactionDOM(transaction) {
+    // GEt sign
+    const sign = transaction.amount < 0 ? "-$" : "+$"
+    const historyList = document.createElement('li');
+
+    // Add class based on value
+    historyList.classList.add(transaction.amount < 0 ? "minus" : "plus");
+    historyList.innerHTML = `
+        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
+        <button class="delete-btn" onclick="removeTransaction(${transaction.id})" /> x </button>
+        `;
+
+    list.appendChild(historyList);
+}
+
+// Display balance, income and expense
+function updateValues() {
+    const amount = transactions.map(item => item.amount);
+    const value = amount.reduce((acc, item) => (acc += item), 0);
+    const income = amount
+        .filter((item) => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+    const expense = (amount
+            .filter((item) => item < 0)
+            .reduce((acc, item) => (acc += item), 0) * -1)
+        .toFixed(2);
+
+    balance.innerText = `$${value}`;
+    moneyPlus.innerText = `$${income}`;
+    moneyMinus.innerText = `$${expense}`;
+
+}
+
+// Remove transcation by ID
+function removeTransaction(id) {
+    console.log(id);
+    transactions = transactions.filter(transaction => transaction.id !== id);
     init();
-})();
+}
+
+// Init  app
+function init() {
+    list.innerHTML = ''
+
+    transactions.forEach(addTransactionDOM);
+    updateValues();
+}
+
+init();
+form.addEventListener('submit', addTransaction);
