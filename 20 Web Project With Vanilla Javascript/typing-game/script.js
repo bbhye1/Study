f2813bacc078ed31c3633d4215f88e6125f44f12
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-08-12 22:15:26 +0900

    Adding timer
    As time goes by, time decreses. and if time is 0s, game is over and end screen comes out.

--- 20 Web Project With Vanilla Javascript/typing-game/script.js
+++ 20 Web Project With Vanilla Javascript/typing-game/script.js
@@ -5,7 +5,6 @@ const word = document.getElementById('word');
 const text = document.getElementById('text');
 const timeElem = document.getElementById('time');
 const scoreElem = document.getElementById('score');
-const reload = document.getElementById('reload');
 const endGameContainer = document.getElementById('end-game-container');
 
 //Init time
@@ -14,7 +13,14 @@ let time = 10;
 //Init score
 let score = 0;
 
+//Focus on text on start
+text.focus();
+
+//Show word in DOM on start
 showWord();
+
+//Starting count down
+const timeInterval = setInterval(updateTime, 1000);
 
 //Generate random word
 async function generateRandomWords() {
@@ -35,12 +41,34 @@ function updateScore() {
     scoreElem.innerText = score;
 }
 
+//Update time 
+function updateTime() {
+    time--;
+    timeElem.innerText = `${time}s`;
+
+    if (time === 0) {
+        clearInterval(timeInterval);
+        gameOver();
+    }
+}
+
+//Game over, show end screen
+function gameOver() {
+    endGameContainer.innerHTML = `
+            <h2>Time ran out</h2>
+            <p>your final score is ${score}</p>
+            <button onclick="location.reload()">Reload</button>`;
+    endGameContainer.style.display = "flex";
+}
+
 //Update score
 text.addEventListener('input', e => {
-    let insertedWord = e.target.value;
+    const insertedWord = e.target.value;
     if (insertedWord === word.innerText) {
         showWord();
         updateScore();
+        time += 2;
+        updateTime();
 
         //Clear
         e.target.value = '';
