8177cbb50b69fb296b4e3b026eb8308cc0c14da9
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-08-15 18:07:34 +0900

    Add drag event listeners
    Add drag event listeners to drop and down list items.

--- 20 Web Project With Vanilla Javascript/sortable-list/script.js
+++ 20 Web Project With Vanilla Javascript/sortable-list/script.js
@@ -51,11 +51,61 @@ async function createList() {
 
             draggableList.appendChild(listItem);
         });
+    addEventListeners();
 }
 createList();
+
+//Drag start
+function dragStart() {
+    dragMovieIndex = +this.closest('li').getAttribute('data-index');
+}
+
+//Drag over
+function dragOver(e) {
+    e.preventDefault();
+}
+
+//Drag drop
+function dragDrop() {
+    const dragEndIndex = +this.closest('li').getAttribute('data-index');
+    swapItems(dragMovieIndex, dragEndIndex);
+    this.closest('li').classList.remove('over');
+}
+
+//Drag enter
+function dragEnter() {
+    this.closest('li').classList.add('over');
+}
+
+//Drag leave
+function dragLeave() {
+    this.closest('li').classList.remove('over');
+}
+
+// Swap list items that are drag and drop
+function swapItems(fromIndex, toIndex) {
+    const itemOne = listItems[fromIndex].querySelector('.draggable');
+    const itemTwo = listItems[toIndex].querySelector('.draggable');
+    listItems[fromIndex].appendChild(itemTwo);
+    listItems[toIndex].appendChild(itemOne);
+}
+
 
 // Event listeners
 function addEventListeners() {
     const draggables = document.querySelectorAll('.draggable');
     const dragListItems = document.querySelectorAll('.draggable-list li');
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', dragStart);
+
+    });
+
+    draggables.forEach(item => {
+        item.addEventListener('dragover', dragOver);
+        item.addEventListener('drop', dragDrop);
+        item.addEventListener('dragenter', dragEnter);
+        item.addEventListener('dragleave', dragLeave);
+    });
+
+
 }
