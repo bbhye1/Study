9d6ac475a150835800a4f2622f94cbdbd51b23b2
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-08-15 21:16:21 +0900

    Create script.js
    Create Basic javascript: speech recognition.

---
+++ 20 Web Project With Vanilla Javascript/speak-number-guess/script.js
@@ -0,0 +1,26 @@
+const msg = document.getElementById('msg');
+
+const randomNum = getRandomNumber();
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition;
+
+// Start recognition and game start
+recognition.start();
+
+// Capture user speak
+function onSpeak(e) {
+    const msg = e.results[0][0].transcript;
+    writeMessage(msg);
+    checkNumber(msg);
+}
+
+
+// Get random number
+function getRandomNumber() {
+    return Math.floor(Math.random() * 100 + 1);
+}
+
+// Speak result 
+recognition.addEventListener('result', onSpeak);
