69dbbf7471f3576b2335346b9d5f9c8e0c23dcd9
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-08-28 19:04:19 +0900

    Update scroll animation - scroll-section-3
    Add all scroll effects depending on the amount of scrolling on the screen.

--- apple-website-interaction-clone/js/main_2.js
+++ apple-website-interaction-clone/js/main_2.js
@@ -87,8 +87,22 @@
             scrollHeight: 0,
             objs: {
                 container: document.querySelector('#scroll-section-3'),
+                canvasCaption: document.querySelector('.canvas-caption'),
+                canvas: document.querySelector('.image-blend-canvas'),
+                context: document.querySelector('.image-blend-canvas').getContext('2d'),
+                images: [],
             },
             values: {
+                imageCount: 2,
+                imageSequence: [0, 1],
+                rect1X: [0, 0, { start: 0, end: 0 }],
+                rect2X: [0, 0, { start: 0, end: 0 }],
+                rectStartY: 0,
+                blendHeight: [0, 0, { start: 0, end: 0 }],
+                canvasScale: [0, 0, { start: 0, end: 0 }],
+                canvasCaption_opacity_in: [0, 1, { start: 0, end: 0 }],
+                canvasCaption_translateY_in: [20, 0, { start: 0, end: 0 }],
+
 
             }
         }
@@ -110,7 +124,7 @@
             }
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`
         }
-        YOffset = pageYOffset;
+        YOffset = window.pageYOffset;
 
         let totalScrollHeight = 0;
         for (let i = 0; i < sceneInfo.length; i++) {
@@ -123,8 +137,17 @@
         document.body.setAttribute('id', `scroll-active-${currentScene}`);
 
         const widthRatio = window.innerWidth / 1920;
-        sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${widthRatio})`;
-        sceneInfo[2].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${widthRatio})`;
+        const heightRatio = window.innerHeight / 1080;
+        let canvasRatio;
+        if (widthRatio <= heightRatio) {
+            canvasRatio = heightRatio;
+        } else {
+            canvasRatio = widthRatio;
+        }
+
+        sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${canvasRatio})`;
+        sceneInfo[2].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${canvasRatio})`;
+
     }
 
     function scrollLoop() {
@@ -142,7 +165,7 @@
         }
         if (YOffset < prevScrollHeight) {
             enterNewScene = true;
-            if (currentScene < 0) return;
+            if (currentScene === 0) return;
             currentScene--;
             document.body.setAttribute('id', `scroll-active-${currentScene}`);
         }
@@ -155,8 +178,8 @@
         const objs = sceneInfo[currentScene].objs;
         const values = sceneInfo[currentScene].values;
         const currentYOffset = YOffset - prevScrollHeight;
-        const ScrollHeight = sceneInfo[currentScene].scrollHeight;
-        const scrollRatio = currentYOffset / ScrollHeight;
+        const scrollHeight = sceneInfo[currentScene].scrollHeight;
+        const scrollRatio = currentYOffset / scrollHeight;
 
         switch (currentScene) {
             case 0:
@@ -249,11 +272,121 @@
                     objs.pinC.style.transform = `scaleY(${calcValues(values.pinC_scaleY, currentYOffset)})`;
                 }
 
+                if (scrollRatio < 0.9) {
+                    const objs = sceneInfo[3].objs;
+                    const values = sceneInfo[3].values;
+                    const widthRatio = window.innerWidth / objs.canvas.width;
+                    const heightRatio = window.innerHeight / objs.canvas.height;
+                    let canvasRatio;
+
+                    if (widthRatio <= heightRatio) {
+                        canvasRatio = heightRatio;
+                    } else {
+                        canvasRatio = widthRatio;
+                    }
+
+                    objs.canvas.style.transform = `scale(${canvasRatio})`;
+                    objs.context.drawImage(objs.images[0], 0, 0);
+
+                    const recalculatedInnerWidth = document.body.offsetWidth / canvasRatio;
+
+                    const whiteRectWidth = recalculatedInnerWidth * 0.15;
+                    values.rect1X[0] = (objs.canvas.width - recalculatedInnerWidth) / 2;
+                    values.rect1X[1] = values.rect1X[0] - whiteRectWidth;
+                    values.rect2X[0] = values.rect1X[0] + recalculatedInnerWidth - whiteRectWidth;
+                    values.rect2X[1] = values.rect2X[0] + whiteRectWidth;
+
+                    // 좌우 흰색 박스 그리기 
+                    objs.context.fillRect(values.rect1X[0], 0, parseInt(whiteRectWidth), objs.canvas.height);
+                    objs.context.fillRect(values.rect2X[0], 0, parseInt(whiteRectWidth), objs.canvas.height);
+                }
+
 
                 break;
 
             case 3:
+                // 가로세로 모두 꽉 차게 하기 위해서 여기서 세팅(계산 필요)
+                const widthRatio = window.innerWidth / objs.canvas.width;
+                const heightRatio = window.innerHeight / objs.canvas.height;
+                let canvasRatio;
 
+                if (widthRatio <= heightRatio) {
+                    canvasRatio = heightRatio;
+                } else {
+                    canvasRatio = widthRatio;
+                }
+
+                objs.canvas.style.transform = `scale(${canvasRatio})`;
+                objs.context.drawImage(objs.images[0], 0, 0);
+
+                // 캔버스 사이즈에 맞춰 가정한 innerWidth
+                const recalculatedInnerWidth = document.body.offsetWidth / canvasRatio;
+
+                if (!values.rectStartY) {
+                    values.rectStartY = objs.canvas.offsetTop + (objs.canvas.height - objs.canvas.height * canvasRatio) / 2;
+                    // values.rectStartY = objs.canvas.getBoundingClientRect().top;
+                    values.rect1X[2].start = (window.innerHeight / 2) / scrollHeight;
+                    values.rect2X[2].start = (window.innerHeight / 2) / scrollHeight;
+                    values.rect1X[2].end = values.rectStartY / scrollHeight;
+                    values.rect2X[2].end = values.rectStartY / scrollHeight;
+                }
+
+                const whiteRectWidth = recalculatedInnerWidth * 0.15;
+                values.rect1X[0] = (objs.canvas.width - recalculatedInnerWidth) / 2;
+                values.rect1X[1] = values.rect1X[0] - whiteRectWidth;
+                values.rect2X[0] = values.rect1X[0] + recalculatedInnerWidth - whiteRectWidth;
+                values.rect2X[1] = values.rect2X[0] + whiteRectWidth;
+
+                // 좌우 흰색 박스 그리기 
+                objs.context.fillRect(parseInt(calcValues(values.rect1X, currentYOffset)), 0, parseInt(whiteRectWidth), objs.canvas.height);
+                objs.context.fillRect(parseInt(calcValues(values.rect2X, currentYOffset)), 0, parseInt(whiteRectWidth), objs.canvas.height);
+
+                if (scrollRatio < values.rect1X[2].end) {
+                    // step 1
+                    objs.canvas.classList.remove('sticky');
+
+                } else {
+                    // step 2
+                    objs.canvas.classList.add('sticky');
+                    objs.canvas.style.top = `${-(objs.canvas.height - objs.canvas.height * canvasRatio) / 2}px`;
+
+                    values.blendHeight[0] = 0;
+                    values.blendHeight[1] = objs.canvas.height;
+                    values.blendHeight[2].start = values.rect1X[2].end;
+                    values.blendHeight[2].end = values.blendHeight[2].start + 0.2;
+
+                    const blendHeight = calcValues(values.blendHeight, currentYOffset);
+
+                    objs.context.drawImage(objs.images[1],
+                        0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight,
+                        0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight
+                    );
+                    // objs.context.drawImage(image, x, y, width, height);
+
+                    if (scrollRatio > values.blendHeight[2].end) {
+                        values.canvasScale[0] = canvasRatio;
+                        values.canvasScale[1] = document.body.offsetWidth / (objs.canvas.width * 1.5);
+                        values.canvasScale[2].start = values.blendHeight[2].end;
+                        values.canvasScale[2].end = values.canvasScale[2].start + 0.2;
+
+                        objs.canvas.style.transform = `scale(${calcValues(values.canvasScale,currentYOffset)})`
+                        objs.canvas.style.marginTop = '0px'
+
+                    }
+
+                    if (scrollRatio > values.canvasScale[2].end && values.canvasScale[2].end > 0) {
+                        objs.canvas.classList.remove('sticky');
+                        objs.canvas.style.marginTop = `${scrollHeight *0.4}px`
+
+                        values.canvasCaption_opacity_in[2].start = values.canvasScale[2].end;
+                        values.canvasCaption_opacity_in[2].end = values.canvasCaption_opacity_in[2].start + 0.1;
+                        values.canvasCaption_translateY_in[2].start = values.canvasScale[2].end;
+                        values.canvasCaption_translateY_in[2].end = values.canvasCaption_translateY_in[2].start + 0.1;
+
+                        objs.canvasCaption.style.opacity = `${calcValues(values.canvasCaption_opacity_in, currentYOffset)}`;
+                        objs.canvasCaption.style.transfrom = `translateY(${calcValues(values.canvasCaption_translateY_in, currentYOffset)}%)`
+                    }
+                }
                 break;
         }
 
@@ -303,15 +436,28 @@
             image.src = `../video2/002/${i+1}.jpg`
             objs2.videoImages.push(image);
         }
+
+        const objs3 = sceneInfo[3].objs;
+        const values3 = sceneInfo[3].values;
+
+        for (let i = 0; i < values3.imageCount; i++) {
+            const image = new Image();
+            image.src = `../images2/coffee${i+1}.jpg`;
+            objs3.images.push(image);
+        }
     }
-    setCanvasImages();
-    window.addEventListener('resize', setLayout);
+
+    window.addEventListener('resize', () => {
+        setLayout();
+        scrollLoop();
+    });
     window.addEventListener('scroll', () => {
         YOffset = window.pageYOffset;
         scrollLoop();
     });
     window.addEventListener('load', () => {
-        setLayout()
+        setLayout();
+        setCanvasImages();
         sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
     });
 
