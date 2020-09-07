d5421b217c6f0a78d4b38f44c9f11fea1db3ee34
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-09-07 22:51:09 +0900

    Create rock-paper-sissors using class

--- react-basic/rock-paper-scissors/RPS.jsx
+++ react-basic/rock-paper-scissors/RPS.jsx
@@ -1,8 +1,113 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 
-class RPS extends Component {
+const rpsCoords = {
+  바위: 0,
+  가위: "-142px",
+  보: "-284px",
+};
+const scores = {
+  가위: 1,
+  바위: 0,
+  보: -1,
+};
+
+const computerChoice = (imgCoord) => {
+  return Object.entries(rpsCoords).find((v) => {
+    return v[1] === imgCoord;
+  })[0];
+};
+
+class RPS extends PureComponent {
+  state = {
+    result: "",
+    imgCoord: 0,
+    score: 0,
+  };
+
+  interval;
+
+  componentDidMount() {
+    this.interval = setInterval(this.changeHand, 100);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  changeHand = () => {
+    const { imgCoord } = this.state;
+    if (imgCoord === rpsCoords.바위) {
+      this.setState({
+        imgCoord: rpsCoords.가위,
+      });
+    } else if (imgCoord === rpsCoords.가위) {
+      this.setState({
+        imgCoord: rpsCoords.보,
+      });
+    } else if (imgCoord === rpsCoords.보) {
+      this.setState({
+        imgCoord: rpsCoords.바위,
+      });
+    }
+  };
+
+  onClickBtn = (choice) => () => {
+    const { imgCoord } = this.state;
+    clearInterval(this.interval);
+    const myScore = scores[choice];
+    const cpuScore = scores[computerChoice(imgCoord)];
+    const diff = myScore - cpuScore;
+
+    if (diff === 0) {
+      this.setState({
+        result: "비겼습니다!",
+      });
+    } else if ([-1, 2].includes(diff)) {
+      this.setState((prevState) => {
+        return {
+          result: "이겼습니다!",
+          score: prevState.score + 1,
+        };
+      });
+    } else {
+      this.setState((prevState) => {
+        return {
+          result: "졌습니다!",
+          score: prevState.score - 1,
+        };
+      });
+    }
+
+    setTimeout(() => {
+      this.interval = setInterval(this.changeHand, 100);
+    }, 1000);
+  };
+
   render() {
-    return <div>hi</div>;
+    const { result, imgCoord, score } = this.state;
+    return (
+      <React.Fragment>
+        <div
+          id="computer"
+          style={{
+            background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`,
+          }}
+        ></div>
+        <div>
+          <button id="rock" className="btn" onClick={this.onClickBtn("바위")}>
+            바위
+          </button>
+          <button id="sissor" className="btn" onClick={this.onClickBtn("가위")}>
+            가위
+          </button>
+          <button id="paper" className="btn" onClick={this.onClickBtn("보")}>
+            보
+          </button>
+        </div>
+        <div>{result}</div>
+        <div>현재 {score}점</div>
+      </React.Fragment>
+    );
   }
 }
 
