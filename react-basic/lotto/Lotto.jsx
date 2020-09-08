c75841ac3c7d538d2b3202576b514c08a91b27b2
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-09-08 15:12:03 +0900

    Create lotto component using class.

--- react-basic/lotto/Lotto.jsx
+++ react-basic/lotto/Lotto.jsx
@@ -1,8 +1,91 @@
 import React, { Component } from "react";
+import Ball from "./Ball";
+
+function getNumbers() {
+  console.log("getNumbers");
+  const candidate = Array(45)
+    .fill()
+    .map((v, i) => i + 1);
+  const shuffle = [];
+  while (candidate.length > 0) {
+    shuffle.push(
+      candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]
+    );
+  }
+  const bonusNumber = shuffle[shuffle.length - 1];
+  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+  return [...winNumbers, bonusNumber];
+}
 
 class Lotto extends Component {
+  state = {
+    winNumbers: getNumbers(),
+    winBalls: [],
+    bonus: null,
+    redo: false,
+  };
+
+  timeouts = [];
+
+  runTimeouts = () => {
+    const { winNumbers } = this.state;
+    for (let i = 0; i < winNumbers.length - 1; i++) {
+      this.timeouts[i] = setTimeout(() => {
+        this.setState((prevState) => {
+          return {
+            winBalls: [...prevState.winBalls, winNumbers[i]],
+          };
+        });
+      }, i * 1000);
+    }
+    this.timeouts[6] = setTimeout(() => {
+      this.setState({
+        bonus: winNumbers[6],
+        redo: true,
+      });
+    }, 7000);
+  };
+
+  componentDidMount() {
+    this.runTimeouts();
+  }
+
+  componentDidUpdate() {
+    const { winBalls } = this.state;
+
+    if (winBalls.length === 0) {
+      this.runTimeouts();
+    }
+  }
+
+  componentWillUnmount() {
+    this.timeouts.forEach((v) => clearTimeout(v));
+  }
+
+  onClickRedo = () => {
+    this.setState({
+      winNumbers: getNumbers(),
+      winBalls: [],
+      bonus: null,
+      redo: false,
+    });
+  };
+
   render() {
-    return <h1>Hello</h1>;
+    const { winBalls, bonus, redo } = this.state;
+    return (
+      <>
+        <div>당첨 숫자</div>
+        <div id="결과창">
+          {winBalls.map((v) => (
+            <Ball key={v} number={v} />
+          ))}
+        </div>
+        <div>보너스!</div>
+        {bonus && <Ball number={bonus} />}
+        {redo && <button onClick={this.onClickRedo}>한 번 더 !</button>}
+      </>
+    );
   }
 }
 
