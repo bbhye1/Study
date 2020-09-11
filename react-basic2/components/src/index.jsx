5930782a6b84acc01fc90805ba2de000b5030f5a
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-09-11 23:29:50 +0900

    Create ApprovalCard.jsx and wrap the CommentDetail.jsx

--- react-basic2/components/src/index.jsx
+++ react-basic2/components/src/index.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        content="Nice blog post!"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Nice blog post!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Bicky"
+          timeAgo="Today at 6:00PM"
+          content="Hello!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sally"
+          timeAgo="Yesterday at 4:45PM"
+          content="You did a great job!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
